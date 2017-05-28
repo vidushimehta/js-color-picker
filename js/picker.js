2c555588b2a4bb7e3065e615f1bb5c5310a5ff9c
vidushimehta <vidushi.2010@gmail.com>
2017-05-29 00:18:58 +0530

    js added for random colors on load

--- js/picker.js
+++ js/picker.js
@@ -1,5 +1,6 @@
 function setSliderValue(slideAmount, id) {
   document.getElementById(id).value=slideAmount;
+
   displayColor();
 }
 
@@ -7,6 +8,23 @@ function displayColor() {
 	var redValue = document.getElementById("red").value;
 	var greenValue = document.getElementById("green").value;
 	var blueValue = document.getElementById("blue").value;
+	
 	document.getElementById("colorbox").style.background ="rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
 }
 
+function setRandomColor() {
+	var redValue = Math.floor(Math.random() * 256);
+	var greenValue = Math.floor(Math.random() * 256);
+	var blueValue = Math.floor(Math.random() * 256);
+
+	document.getElementById("red").value= redValue;
+	document.getElementById("red_text").value = redValue;
+	document.getElementById("green").value= greenValue;
+	document.getElementById("green_text").value= greenValue;
+	document.getElementById("blue").value = blueValue;
+	document.getElementById("blue_text").value= greenValue;
+
+	displayColor();
+}
+
+window.onload = setRandomColor;
