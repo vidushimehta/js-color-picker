bc36a8328d76c785ed47cb9e5d3e3a0f37a3680f
vidushimehta <vidushi.2010@gmail.com>
2017-05-28 23:33:07 +0530

    js added for displaying color

--- js/picker.js
+++ js/picker.js
@@ -1,3 +1,12 @@
 function setSliderValue(slideAmount, id) {
   document.getElementById(id).value=slideAmount;
+  displayColor();
 }
+
+function displayColor() {
+	var redValue = document.getElementById("red").value;
+	var greenValue = document.getElementById("green").value;
+	var blueValue = document.getElementById("blue").value;
+	document.getElementById("colorbox").style.background ="rgb(" + redValue + "," + greenValue + "," + blueValue + ")";
+}
+
